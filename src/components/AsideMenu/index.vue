9fd74714c79f6219feefe59e8ee17a71e4c4b922
wuyong <wu4yong@163.com>
2023-03-28 01:12:49 +0800

    icon修改

--- src/components/AsideMenu/index.vue
+++ src/components/AsideMenu/index.vue
@@ -5,25 +5,25 @@
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
       <el-menu :default-openeds="['1', '3']">
         <el-submenu index="1">
-          <template slot="title"><i class="el-icon-message"></i>用户管理</template>
+          <template slot="title"><i class="el-icon-user"></i>用户管理</template>
           <el-menu-item-group>
-            <el-menu-item index="1-1">用户列表</el-menu-item>
-            <el-menu-item index="1-2">信息修改</el-menu-item>
+            <el-menu-item index="1-1"><i class="el-icon-s-operation"></i>用户列表</el-menu-item>
+            <el-menu-item index="1-2"><i class="el-icon-edit"></i>信息修改</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
         <el-submenu index="2">
-          <template slot="title"><i class="el-icon-message"></i>数据管理</template>
+          <template slot="title"><i class="el-icon-s-data"></i>数据管理</template>
           <el-menu-item-group>
-            <el-menu-item index="1-1">表管理</el-menu-item>
-            <el-menu-item index="1-2">数据管理</el-menu-item>
+            <el-menu-item index="1-1"><i class="el-icon-setting"></i>表管理</el-menu-item>
+            <el-menu-item index="1-2"><i class="el-icon-s-tools"></i>数据管理</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
 
         <el-submenu index="3">
-          <template slot="title"><i class="el-icon-message"></i>会员管理</template>
+          <template slot="title"><i class="el-icon-monitor"></i>会员管理</template>
           <el-menu-item-group>
-            <el-menu-item index="1-1">会员人数</el-menu-item>
-            <el-menu-item index="1-2">收支明细</el-menu-item>
+            <el-menu-item index="1-1"><i class="el-icon-user-solid"></i>会员人数</el-menu-item>
+            <el-menu-item index="1-2"><i class="el-icon-goods"></i>收支明细</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
       </el-menu>
