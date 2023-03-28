89277334f13ddf7408533123005fde73dfa0ae2c
wuyong <wu4yong@163.com>
2023-03-28 11:23:08 +0800

    修改导航栏

--- src/components/NavBar/index.vue
+++ src/components/NavBar/index.vue
@@ -1,8 +1,13 @@
 <!-- 页面区域 -->
 <template>
   <el-header>
-    <span class="span">
-      <font size="3">欢迎 <font style="color:red">管理员</font> 登录系统</font>
+    <span class="span1">
+      <img class="img" src="@/assets/logo.png" />
+    </span>
+    <span class="span2">
+      <font size="3">欢迎 <font style="color: red">管理员</font> 登录系统</font>
+    </span>
+    <span class="span3">
       <el-button class="button" size="mini" type="primary">登录</el-button>
       <el-button class="button" size="mini" type="success">注册</el-button>
     </span>
@@ -25,7 +30,23 @@ export default {
   line-height: 40px;
 }
 
-.span {
+.img {
+  height: 45px;
+  width: 45px;
+}
+
+.span1 {
+  margin-top: 12px;
+  margin-left: 12px;
+  float: left;
+}
+
+.span2 {
+  margin-top: 12px;
+  float: left;
+}
+
+.span3 {
   margin-top: 12px;
   float: right;
   color: rgb(25, 8, 182);
