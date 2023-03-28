42723fe42e57e3e2c28ade9c68b9612cf5032d26
wuyong <wu4yong@163.com>
2023-03-28 14:48:04 +0800

    表格样式调整

--- src/pages/Home/ManagerData/Add/index.vue
+++ src/pages/Home/ManagerData/Add/index.vue
@@ -4,14 +4,31 @@
     <el-main>
       <!-- 表单 -->
       <h1 class="h1">新建表</h1>
-      <el-form :inline="true" class="el-form" ref="form" label-width="80px" size="mini">
+      <el-form :inline="true" class="el-form" ref="form" label-width="auto" size="mini">
+
         <!-- 集群名称 -->
         <el-form-item label="集群名称:">
-          <el-input v-model.trim="tableInfo.clusterName"></el-input>
+          <el-input v-model.trim="tableInfo.clusterName" :value="tableInfo.clusterName" disabled></el-input>
         </el-form-item>
+        <!-- 业务部门 -->
+        <el-form-item label="业务部门:">
+          <el-input v-model.trim="tableInfo.businessDepart" disabled></el-input>
+        </el-form-item>
+        <!-- 管理员 -->
+        <el-form-item label="管理员:">
+          <el-input v-model.trim="tableInfo.manager" disabled></el-input>
+        </el-form-item>
+        <!-- 样本数据 -->
+        <el-form-item label="样本数据:">
+          <el-input v-model.trim="tableInfo.sampleData" disabled></el-input>
+        </el-form-item>
+
         <!-- 命名空间 -->
         <el-form-item label="命名空间:">
-          <el-input v-model.trim="tableInfo.namespace"></el-input>
+          <el-select v-model.trim="tableInfo.namespace" placeholder="请选择">
+            <el-option v-for="item in options" :key="item.value" :label="item.label" :value="tableInfo.value">
+            </el-option>
+          </el-select>
         </el-form-item>
         <!-- 表名称 -->
         <el-form-item label="表名称:">
@@ -22,20 +39,8 @@
           <el-input v-model.trim="tableInfo.columnFamilyName"></el-input>
         </el-form-item>
         <!-- TTL(保存时间:秒) -->
-        <el-form-item label="TTL(保存时间:秒):">
+        <el-form-item label="TTL时间(秒):">
           <el-input v-model.trim="tableInfo.saveTime"></el-input>
-        </el-form-item>
-        <!-- 业务部门 -->
-        <el-form-item label="业务部门:">
-          <el-input v-model.trim="tableInfo.businessDepart"></el-input>
-        </el-form-item>
-        <!-- 管理员 -->
-        <el-form-item label="管理员:">
-          <el-input v-model.trim="tableInfo.manager"></el-input>
-        </el-form-item>
-        <!-- 样本数据 -->
-        <el-form-item label="样本数据:">
-          <el-input v-model.trim="tableInfo.sampleData"></el-input>
         </el-form-item>
 
         <el-form-item>
@@ -50,25 +55,14 @@
       <!-- 表格区域 -->
       <el-footer>
         <el-table :data="tableList">
-          <el-table-column v-for="{ prop, label } in colConfigs" :key="prop" :prop="prop" :label="label">
+          <el-table-column v-for="{ prop, label } in tableHeaderConfigs" :key="prop" :prop="prop" :label="label">
           </el-table-column>
         </el-table>
-        <!-- 
-          <el-table>
-          <el-table-column prop="clusterName" label="集群名称"> </el-table-column>
-          <el-table-column prop="namespace" label="命名空间"> </el-table-column>
-          <el-table-column prop="tableName" label="表名称"> </el-table-column>
-          <el-table-column prop="columnFamilyName" label="列族名称"> </el-table-column>
-          <el-table-column prop="saveTime" label="TTL(保存时间:秒):"> </el-table-column>
-          <el-table-column prop="businessDepart" label="业务部门"> </el-table-column>
-          <el-table-column prop="manager" label="管理员"> </el-table-column>
-          <el-table-column prop="sampleData" label="样本数据"> </el-table-column>
-          <el-table-column prop="createDate" label="创建日期"> </el-table-column>
-        </el-table> 
-        -->
       </el-footer>
+
     </el-main>
   </el-container>
+
 </template>
 
 <!-- 组件区域 -->
@@ -78,8 +72,8 @@ import { mapState } from "vuex";
 
 export default {
   name: "manager-add",
-  // 通过Vuex发起ajax请求，将数据存储在store仓库中（此处用的mock请求）
   mounted() {
+    // 通过vuex发起ajax请求，将数据存储在store仓库中（此处暂时使用mock请求数据）
     this.$store.dispatch("getTableInfoList");
   },
   computed: {
@@ -88,62 +82,46 @@ export default {
     }),
   },
   data() {
-    // this.colConfigs = [
-    //   { prop: 'date', label: '日期' },
-    //   { prop: 'name', label: '姓名' },
-    //   { prop: 'address', label: '地址' }
-    // ]
-    // 表单数据
-
-    // tableData: [
-    //   {
-    //     date: '2016-05-02',
-    //     name: '王小虎',
-    //     address: '上海市普陀区金沙江路 1518 弄'
-    //   }, {
-    //     date: '2016-05-04',
-    //     name: '王小虎',
-    //     address: '上海市普陀区金沙江路 1517 弄'
-    //   }
-    // ],
-    this.colConfigs = [
+    // 列表表头配置
+    this.tableHeaderConfigs = [
       { prop: 'clusterName', label: '集群名称' },
       { prop: 'namespace', label: '命名空间' },
       { prop: 'tableName', label: '表名称' },
       { prop: 'columnFamilyName', label: '列族名称' },
-      { prop: 'saveTime', label: 'TTL(保存时间:秒)' },
+      { prop: 'saveTime', label: 'TTL时间(秒)' },
       { prop: 'businessDepart', label: '业务部门' },
       { prop: 'manager', label: '管理员' },
       { prop: 'sampleData', label: '样本数据' },
       { prop: 'createDate', label: '创建日期' },
     ]
     return {
-      tableInfo: {
-
-      },
       options: [
         {
-          value: "选项1",
-          label: "黄金糕",
+          value: "01",
+          label: "default",
         },
         {
-          value: "选项2",
-          label: "双皮奶",
+          value: "02",
+          label: "user",
         },
         {
-          value: "选项3",
-          label: "蚵仔煎",
-        },
-        {
-          value: "选项4",
-          label: "龙须面",
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭",
-        },
+          value: "03",
+          label: "company",
+        }
       ],
-      value: "",
+      value: 'default',
+      // 列表数据对象
+      tableInfo: {
+        "clusterName": "华为服务器01",
+        "namespace": "default",
+        "tableName": '',
+        "columnFamilyName": '',
+        "saveTime": '',
+        "businessDepart": '研发部',
+        "manager": 'admin',
+        "sampleData": '暂无数据',
+        "createDate": ''
+      }
     };
   },
   methods: {
@@ -171,8 +149,7 @@ export default {
   height: 100%;
 } */
 .el-form {
-  /* margin-top: 10px; */
   padding-top: 5px;
-  border: 0.5px solid rgb(136, 136, 160);
+  border: 0.05em dashed rgb(136, 136, 160);
 }
 </style>
